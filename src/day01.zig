78c321b1f6cd8deed775fb6518465527b1bc0c03
famuyiwadayo <famuyiwadayodaniel@gmail.com>
2022-08-29 22:57:05 +0100

    finished aoc 2020 day01

--- src/day01.zig
+++ src/day01.zig
@@ -11,8 +11,64 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day01.txt");
 
+// https://adventofcode.com/2020/day/1
 pub fn main() !void {
+    // print("Hello world", .{});
 
+    const numbers = try getNumbers();
+
+    const partOneValues = try partOne(numbers);
+    const partTwoValues = try partTwo(numbers);
+
+    print("{any} ----- {any}\n", .{ partOneValues, partTwoValues });
+}
+
+fn partTwo(numbers: []usize) ![]usize {
+    var values = List(usize).init(gpa);
+
+    for (numbers) |n0, i| {
+        for (numbers[i + 1 ..]) |n1, j| {
+            if (i + 1 == numbers.len + 1) @panic("Bad loop!!");
+            for (numbers[j + 1 ..]) |n2| {
+                if (j + 1 == numbers.len + 1) @panic("Bad loop!!");
+                if (n0 + n1 + n2 == 2020) {
+                    print("PART TWO --> Found the numbers, {} * {} * {} = {}\n", .{ n0, n1, n2, n0 * n1 * n2 });
+                    try values.appendSlice(&[3]usize{ n0, n1, n2 });
+                    return values.toOwnedSlice();
+                }
+            }
+        }
+    }
+
+    return values.toOwnedSlice();
+}
+
+fn partOne(numbers: []const usize) ![]usize {
+    var values = List(usize).init(gpa);
+    for (numbers) |n0, i| {
+        for (numbers[i + 1 ..]) |n1| {
+            if (i + 1 == numbers.len + 1) @panic("Bad loop!!");
+            if (n0 + n1 == 2020) {
+                print("PART ONE --> Found the numbers, {} * {} = {}\n", .{ n0, n1, n0 * n1 });
+                try values.appendSlice(&[2]usize{ n0, n1 });
+                return values.toOwnedSlice();
+            }
+        }
+    }
+
+    return values.toOwnedSlice();
+}
+
+fn getNumbers() ![]usize {
+    var numbers = List(usize).init(gpa);
+    var line_it = tokenize(u8, data, "\n");
+
+    while (line_it.next()) |line| {
+        const num = try parseInt(usize, line, 10);
+        try numbers.append(num);
+    }
+
+    return numbers.toOwnedSlice();
 }
 
 // Useful stdlib functions
